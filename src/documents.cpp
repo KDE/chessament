a06663ad05785549fc0614b5ece3144b1790051c
Manuel Alcaraz Zambrano <manuel@alcarazzam.dev>
2026-05-08 11:09:49 +0200

    Rename config file to be less generic

--- src/documents.cpp
+++ src/documents.cpp
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2024-2025 Manuel Alcaraz Zambrano <manuel@alcarazzam.dev>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "config.h"
+#include "chessamentconfig.h"
 #include "controller.h"
 #include "document.h"
 #include "playersmodel.h"
